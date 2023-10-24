6e2436efc82055d4822dc80694ce082e444c1a21
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-10-24 19:18:10 -0400

    styling of a links

--- app/_components/nav.tsx
+++ app/_components/nav.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "@styles/page.module.scss";
 import { useColors } from "../_hooks";
+import Link from "next/link";
 
 const navItems = ["Home", "AI", "Projects", "Contact", "About"];
 
@@ -13,13 +14,13 @@ export default function Nav() {
     <>
     <nav className={styles.nav}>
       {navItems.map((item, i) => (
-        <a
+        <Link
           key={item}
           href={item !== 'Home' ? `/${item.toLowerCase()}` : '/'}
           style={{ color: `${title}` }}
         >
           {item}
-        </a>
+        </Link>
       ))}
     </nav>
     </>
