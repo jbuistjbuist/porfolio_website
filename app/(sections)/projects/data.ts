9dd5b44082c28bdeed8e154fedd9b0eb2d73898c
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-10-26 20:06:34 -0400

    remove unused files

---
+++ app/(sections)/projects/data.ts
@@ -0,0 +1,31 @@
+export const projectsList = [
+  {
+    title: "Portfolio Website",
+    href: "/projects/portfolio",
+    shortDescription: "The website you are on right now!",
+    techStack: ["React", "NextJS", "Three.js", "SASS", "CircleCI", "Jest", "Typescript", "Vercel"]
+  }, 
+  { 
+    title: "Titan Exercise",
+    href: "/projects/titan-exercise",
+    shortDescription: "React app to create and track your workouts",
+    techStack: ["React", "Ruby", "Rails", "PostgreSQL", "react-spring", "Storybook", "Jest", "Heroku"]
+  },
+  { title: "Basic Checks",
+    href: "/projects/basic-checks",
+    shortDescription: "Python script with Selenium integrated with Google Sheets to automate workflows",
+    techStack: ["Python", "Selenium", "Google Cloud Platform", "Zendesk API"]
+  },
+  {
+    title: "Quizzer App",
+    href: "/projects/quizzer-app",
+    shortDescription: "App built with jQuery and EJS for creating, taking, and sharing quizzes",
+    techStack: ["Javascript", "SASS", "EJS", "Node.js"]
+  },
+  {
+    title: "Form Cache",
+    href: "/projects/form-cache",
+    shortDescription: "Chrome extension for temporarily caching form data in case of reload/navigation",
+    techStack: ["Javascript", "CSS3", "HTML5", "Chrome APIs"]
+  }
+]
--- app/(sections)/projects/page.tsx
+++ app/(sections)/projects/page.tsx
@@ -1,12 +1,33 @@
+import { projectsList } from "./data"
+import Link from "next/link";
+import Image from "next/image";
 
 
 export default function Projects() {
+
+
   return (
-    <section id="projects" aria-label="projects section" style={{height: '100vh'}}>
-      projects
-      sdlkfjsldkfjj
-      sldkfjslkdfj
-      sldkfjsl
+    <section id="projects" aria-label="projects section">
+      <h1>Projects</h1>
+      {projectsList.map((project) => {
+        const {title, shortDescription, href, techStack} = project;
+
+        return (
+          <Link href={href}>
+            <h2>{title}</h2>
+            <p>{shortDescription}</p>
+            <p>
+              {
+                project.techStack?.map((str) => {
+                  return (
+                    <span>{str}</span>
+                  )
+                })
+              }
+            </p>
+          </Link>
+        )
+      })}
     </section>
   )
 }
